42d44e8c19b8ff40da77e5ecdf325a8996a16fe3
Amit Sharma <amits781@gmail.com>
2023-09-22 01:12:26 +0530

    version update

--- react_app/reactServer.js
+++ react_app/reactServer.js
@@ -53,7 +53,7 @@ app.get('/iot-actuator/health', (req, res) => {
   // You can add more complex health checks here if needed
   const healthStatus = {
     status: 'UP',
-    version: '1.0.0',
+    version: '1.0.1',
   };
   res.json(healthStatus);
 });
