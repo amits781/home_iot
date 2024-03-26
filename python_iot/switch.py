854e667165088138d38fa60ab614bf6fd8696fdd
Amit Sharma <amits781@gmail.com>
2024-03-26 17:05:44 +0530

    bug fixed

--- python_iot/switch.py
+++ python_iot/switch.py
@@ -79,6 +79,7 @@ async def check_device_status_periodically(interval=30):
                     # Update the SinricPro about the current status.
                     iotDeviceStatus = SinricProConstants.POWER_STATE_OFF if motorStatus == 0 else SinricProConstants.POWER_STATE_ON
                     client.event_handler.raise_event(SWITCH_ID, SinricProConstants.SET_POWER_STATE, data = {SinricProConstants.STATE: iotDeviceStatus })
+                    motorStateLocal = motorStatus
             else:
                 logger.error(f"Failed to check motor status. HTTP Error: {response.status_code}")
                 client.event_handler.raise_event(SWITCH_ID, SinricProConstants.SET_POWER_STATE, data = {SinricProConstants.STATE: SinricProConstants.POWER_STATE_OFF })
