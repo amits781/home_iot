676b3e66f9b650244d7c9b4f5a0174747cfa227e
Amit Sharma <amits781@gmail.com>
2024-07-30 02:21:40 +0530

    added some animation

--- booting/script.js
+++ booting/script.js
@@ -6,14 +6,19 @@ async function checkHealth(url, elementId, checkStatus) {
         try {
             const response = await fetch(url);
             if (response.status === 200) {
+                
                 healthSpan.textContent = 'UP';
-                healthSpan.classList.add('glow-green');
+                healthSpan.parentElement.classList.remove('status-fail')
+                healthSpan.parentElement.classList.add('status-success')
                 healthSpan.classList.remove('blinking-text');
+                healthSpan.classList.add('glow-green');
                 healthSpan.style.color = '#207e3c'; // Turn text green on success
                 healthSpan.style.fontWeight = 900;
                 clearInterval(intervalId); // Stop further checks
                 checkStatus[elementId] = true; // Update check status
-
+                checkStatus['start'] = checkStatus['end'];
+                checkStatus['end'] = checkStatus['end'] + (100/checkStatus['checkCount']);
+                move(checkStatus);
                 // Check if both health checks are successful
                 if (checkStatus['health-frontend'] && checkStatus['health-backend']) {
                     window.location.href = 'http://localhost:3000';
@@ -30,11 +35,29 @@ async function checkHealth(url, elementId, checkStatus) {
     }, 5000); // Check every 5 seconds
 }
 
+function move(checkStatus) {
+    var elem = document.getElementById("myBar");   
+    var width = checkStatus['start'];
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= checkStatus['end']) {
+        clearInterval(id);
+      } else {
+        width++; 
+        var value = '0 ' + (100 - width) +'% 0 0'
+        elem.style.setProperty('--inset-value', value);
+      }
+    }
+  }
+
 // Call the function when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     const checkStatus = {
         'health-frontend': false,
-        'health-backend': false
+        'health-backend': false,
+        'start': 0,
+        'end': 0,
+        'checkCount': 2
     };
 
     checkHealth('http://localhost:3000/iot-actuator/health', 'health-frontend', checkStatus);
