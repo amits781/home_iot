ac97e49727bbbdf4000f65c25e9657c7814ae4ab
amits781 <35662110+amits781@users.noreply.github.com>
2025-08-08 09:20:12 +0530

    Change internal IP

--- spring_boot_service/src/main/java/com/aidyn/iot/utils/MotorConstants.java
+++ spring_boot_service/src/main/java/com/aidyn/iot/utils/MotorConstants.java
@@ -2,7 +2,7 @@ package com.aidyn.iot.utils;
 
 public class MotorConstants {
 
-  public final static String ARDUINO_HOST = "http://192.168.1.21";
+  public final static String ARDUINO_HOST = "http://home.arduino";
   public final static String TURN_ON_API = "H";
   public final static String TURN_OFF_API = "L";
   public final static String STATUS_API = "S";
